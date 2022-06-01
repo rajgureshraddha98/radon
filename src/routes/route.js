b53031ff8dfe85ca612cacc0b6f42413897313ef
rajgureshraddha98 <shraddha.rajguru99@gmail.com>
2022-06-01 23:39:11 +0530

    module

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
+const shalu = require('.logger/logger')
+const shalu1 = require('../validation/formatter')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+
+
+
+router.get('/test-me', function(req, res) {
+    shalu.welcome()
     res.send('My first ever api!')
 });
 
