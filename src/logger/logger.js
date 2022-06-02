0fbb7a233731c5604148aef6876b0ae8d09375cd
rajgureshraddha98 <shraddha.rajguru99@gmail.com>
2022-06-02 23:14:57 +0530

    shalu

---
+++ src/logger/logger.js
@@ -0,0 +1,8 @@
+const welcome = function() {
+
+
+
+    console.log("Welcome to my application. I am shraddha rajgure and a part of FunctionUp Thorium cohort")
+
+}
+module.exports.welcome = welcome;
