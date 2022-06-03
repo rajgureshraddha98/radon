a627a6548a202728066afa6f8765e93412cb5662
rajgureshraddha98 <shraddha.rajguru99@gmail.com>
2022-06-03 09:25:44 +0530

    msg

--- src/routes/route.js
+++ src/routes/route.js
@@ -72,6 +72,120 @@ router.get('/candidates/:canidatesName', function(req, res) {
     res.send('Done')
 })
 
+router.get("/solution1", function(req, res) {
+    var a = [1, 2, 3, 5, 6, 7],
+        count = a[a.length - 1];
+    var missing = [];
+    for (var i = 1; i <= count; i++) {
+        if (a.indexOf(i) == -1) {
+            missing.push(i);
+        }
+    }
+    console.log(missing.toString());
+    res.send('done')
+})
+
+//2
+router.get("/solution2", function(req, res) {
+    var a = [33, 34, 35, 37, 38],
+        count = a[a.length - 1];
+    var missing = [];
+    for (var i = 33; i <= count; i++) {
+        if (a.indexOf(i) == -1) {
+            missing.push(i);
+        }
+    }
+    console.log(missing.toString());
+    res.send('done')
+})
+
+router.get('/movies', function(req, res) {
+    // console.log('The request objects is '+ JSON.stringify(req.params))
+    // console.log('Movies name is '+req.params.moviesName)
+    let movies = ["‘Rang de basanti’, ‘The shining’, ‘Lord of the rings’, ‘Batman begins’"]
+    console.log(movies)
+    res.send('done')
+
+})
+
+//2 & 3
+router.get('/movies/:indexNumber', function(req, res) {
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let movieIndex = req.params.indexNumber
+    let finalMovie = ""
+    if (movieIndex < movies.length) {
+        finalMovie = movies[movieIndex];
+    } else {
+        finalMovie = ("the movie with index no. doesnt exist")
+    }
+    console.log(movies.length)
+    console.log('The request objects is ' + JSON.stringify(req.params))
+    console.log('Movies name is ' + req.params.moviesName)
+    res.send(finalMovie)
+})
+
+router.get('/animals/:indexNumber', function(req, res) {
+    let animals = ["cat", "dog", "lion", "cheetah", "bear", "elephant"]
+    let animalsIndex = req.params.indexNumber;
+    let animalFinal = ""
+    if (animalsIndex < animals.length) {
+        animalFinal = animals[animalsIndex];
+    } else {
+        animalFinal = ('the animal not present')
+    }
+    console.log(animals.length)
+    console.log('the object requested is' + JSON.stringify(req.params))
+    console.log('animal name is' + req.params.animalsNames)
+    res.send(animalFinal)
+})
+
+//4
+router.get("/sol2", function(req, res) {
+    let a = [{
+            id: 1,
+            name: "The Shining"
+        },
+        { id: 2, name: "Incendies" },
+        { id: 3, name: "Rang de Basanti" },
+        { id: 4, name: "Finding Nemo" }
+    ];
+
+    res.send(a)
+})
+
+
+//5
+router.get('/films/:indexNumber', function(req, res) {
+    let films = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incedies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+
+    let movieIndex = req.params.indexNumber;
+    let finalMovie = "";
+    if (movieIndex < films.length) {
+        finalMovie = films[movieIndex];
+    } else {
+        finalMovie = ("the movie with index no. doesnt exist")
+    }
+    console.log(films.length)
+    console.log('The request objects is ' + JSON.stringify(req.params))
+    console.log('Movies name is ' + req.params.moviesName)
+    res.send(finalMovie)
+})
+
+
+
+
 
 module.exports = router;
 // adding this comment for no reason
