fac7fd7d0121bc73121f99137ceafe4ffc3b787f
rajgureshraddha98 <shraddha.rajguru99@gmail.com>
2022-06-05 12:00:12 +0530

    msg

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,14 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
-let players = [{
-        "name": "manish",
-        "dob": "1/1/1995",
-        "gender": "male",
-        "city": "jalna",
-        "sports": ["swimming"]
-    },
 
-    {
-        "name": "gopal",
-        "dob": "11/12/1992",
-        "gender": "male",
-        "city": "pune",
-        "sports": ["soccer"]
+const UserController = require('../controllers/userController')
+const UserModel = require('../models/userModel')
 
-    },
-    {
+router.get('/test-me', function(req, res) {
 
-        "name": "lokesh",
-        "dob": "1/1/1990",
-        "gender": "male",
-        "city": "mumbai",
-        "sports": ["swimming"]
-    }
-];
-
-router.post("/players", function(req, res) {
-
-    const d = req.body
-    players.push(d)
-    console.log(players)
-
-    res.send({ data: players })
+    res.send("hi shalmali welcome")
 });
+router.post('/createUser', userController.createUser);
+router.get('/getAllUsers', userController.getUsersData);
 
 module.exports = router;
