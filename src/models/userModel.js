a8c18c967ff672325114ae1633d6f1fb5802d02b
rajgureshraddha98 <shraddha.rajguru99@gmail.com>
2022-06-07 13:27:15 +0530

    sesion2

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,33 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookModel1 = new mongoose.Schema({
+    bookName: {
         type: String,
-        unique: true,
         required: true
     },
-    emailId: String,
-    gender: {
+    authorName: String,
+
+    tags: [String],
+
+
+    year: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        default: 2021
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+
+
+
+    price: {
+        indianPrice: String,
+        europePrice: String,
+    },
+
+
+    totalPages: Number,
+
+    stockAvailable: Boolean
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
 
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model('newBooks', bookModel1)
